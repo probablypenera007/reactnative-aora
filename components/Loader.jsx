b40849a970458101a3144879225ed8fb8e36bf36
James Penera <jamespenera@icloud.com>
2024-05-04 01:09:40 +0400

    Update Loader.jsx

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -2,6 +2,11 @@ import { View, ActivityIndicator, Dimensions, Platform } from "react-native";
 
  
  const Loader = ({ isLoading }) => {
+    const osName = Platform.OS;
+    const screenHeight = Dimensions.get("screen").height;
+  
+    if (!isLoading) return null;
+    
    return (
      <View
      className="absolute flex justify-center items-center w-full h-full bg-primary/60 z-10"
@@ -9,7 +14,11 @@ import { View, ActivityIndicator, Dimensions, Platform } from "react-native";
        height: screenHeight,
      }}
      >
-       <Text>Loader</Text>
+        <ActivityIndicator
+        animating={isLoading}
+        color="#fff"
+        size={osName === "ios" ? "large" : 50}
+      />
      </View>
    )
  }
