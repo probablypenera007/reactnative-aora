9fea5ed9ae76e3cbb8ac890bb1e3ffcce4fec237
James Penera <jamespenera@icloud.com>
2024-05-04 01:11:04 +0400

    Update FormField.jsx

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,7 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({
+    title,
+    value,
+    placeholder,
+    handleChangeText,
+    otherStyles,
+    ...props
+}) => {
   return (
     <View>
       <Text>FormField</Text>
