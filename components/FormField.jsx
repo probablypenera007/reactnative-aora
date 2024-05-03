283b5e5b53b08404b1d47d92d0e9b88cdd5f5edd
James Penera <jamespenera@icloud.com>
2024-05-04 02:28:18 +0400

    merge folder

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -16,6 +16,7 @@ const FormField = ({
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+
       <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
         <TextInput
           className="flex-1 text-white font-psemibold text-base"
@@ -26,6 +27,7 @@ const FormField = ({
           secureTextEntry={title === "Password" && !showPassword}
           {...props}
         />
+
         {title === "Password" && (
           <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
             <Image
