75ff9666d109a4b0a2901f7f7fb1464c3ad74e3e
James Penera <jamespenera@icloud.com>
2024-05-04 01:08:09 +0400

    Update Loader.jsx

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,1 +1,12 @@
 import { View, ActivityIndicator, Dimensions, Platform } from "react-native";
+
+ 
+ const Loader = () => {
+   return (
+     <View>
+       <Text>Loader</Text>
+     </View>
+   )
+ }
+ 
+ export default Loader
