7af39522447841e50600bec3ceafbbce1ab38aeb
James Penera <jamespenera@icloud.com>
2024-05-04 00:10:28 +0400

    Update _layout.jsx

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,6 +1,26 @@
 import { View, Text } from 'react-native'
 import {Tabs, Redirect} from 'expo-router'
 
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+
 const TabsLayout = () => {
   return (
     <>
