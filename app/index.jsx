c9f62c7d8131a116f35c9df03f6789769878c8d7
James Penera <jamespenera@icloud.com>
2024-05-03 19:25:06 +0400

    profiile created

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Link} from 'expo-router'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hi ProbablyPenera! Welcome to Aora.</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'limegreen'}} >Go to profile</Link>
     </View>
   );
 }
