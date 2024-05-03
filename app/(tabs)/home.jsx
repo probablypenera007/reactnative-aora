4b9a8c810518367d5ed5c813c8bb78485a14ea38
James Penera <jamespenera@icloud.com>
2024-05-04 03:39:41 +0400

    Update home.jsx

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
           />
         )}
         ListHeaderComponent={() => (
-          <View className="flex my-6 px-4 space-y-6">
+          <View className="flex my-2 px-4 space-y-20">
             <View className="flex justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
