3db9d1c94f3efccbf9224636ed629121470de8b3
James Penera <jamespenera@icloud.com>
2024-05-03 23:42:41 +0400

    splashscreen

--- app/index.jsx
+++ app/index.jsx
@@ -4,8 +4,8 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl" >Hi ProbablyPenera! Welcome to Aora.</Text>
+    <View className="flex-1 items-center justify-center bg-primary">
+      <Text className="text-3xl" >Hi ProbablyPenera!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "limegreen" }}>
         Go to profile
