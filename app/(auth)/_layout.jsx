82f720b42c11a05791be9dfbf5616034257f6817
James Penera <jamespenera@icloud.com>
2024-05-04 01:06:35 +0400

    Update _layout.jsx

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,27 @@
 import { View, Text } from 'react-native'
-import { Stack } from "expo-router"
+import { Stack, Redirect } from "expo-router"
+import { StatusBar } from "expo-status-bar";
 
 const AuthLayout = () => {
   return (
    <>
-   <Stack.Screen
-   />
+    <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+
+      <Loader isLoading={loading} />
+      <StatusBar backgroundColor="#161622" style="light" />
    </>
   )
 }
